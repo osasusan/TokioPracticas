b4294f8b53abeb32a96bc6dc695e4e258b7c4a99
Osasu Sanachez <osasusan@gmail.com>
2025-11-07 20:59:56 +0100

    Practica 4
     Stack Views

---
+++ Practica 4/Practica 4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica 4
+//
+//  Created by Osasu sanchez on 7/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
